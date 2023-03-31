3c2bfbf54a81f60ac511222bd627b0b46873a207
Crowdbotics <engineering@crowdbotics.com>
2023-03-31 00:20:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wispy_frog_dev_73312/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wispy_frog_dev_73312;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wispy_frog_dev_73312";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-frog-dev-73312",
+  url: "https://wispy-frog-dev-73312.botics.co",
+  api: "https://wispy-frog-dev-73312.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
